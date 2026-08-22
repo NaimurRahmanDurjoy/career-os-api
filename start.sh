924e4b940a107a37ce482999e41f8a45030ec85c
Md. Naimur Rahman <75179160+NaimurRahmanDurjoy@users.noreply.github.com>
2026-08-22 14:46:11 +0600

    Update start.sh to run queue worker

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ php artisan migrate --force
 # Start php-fpm in background
 php-fpm -D
 
+# Start Laravel queue worker in background
+php artisan queue:work --tries=3 --timeout=90 &
+
 # Start NGINX in foreground
 nginx -g "daemon off;"
